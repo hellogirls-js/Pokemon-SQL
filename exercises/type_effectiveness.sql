234b91c35207d12ea76f051b8ccade476d635383
hellogirls-js <neeneemille@gmail.com>
2025-12-28 12:56:14 -0600

    start writing exercises for type effectiveness

---
+++ exercises/type_effectiveness.sql
@@ -0,0 +1,30 @@
+/*
+Type Effectiveness
+
+As a basic math and data retrieval exercise, you will calculate the
+effectiveness of different types and type combinations.
+
+You will start with retrieving the weaknesses and resistances for single types
+then move onto calculating the total type multiplier for various type combinations.
+
+Good luck!
+
+*/
+
+-- Get all of the Grass type's weaknesses. Only return the names of the types.
+
+
+
+-- Fire's weaknesses:
+
+
+
+-- Now, get all of Ice's resistances.
+
+
+
+
+-- Return the TOTAL NUMBER of types the Steel type resists.
+
+
+
