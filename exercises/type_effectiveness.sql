fc41f28e4621b26ede60d37740be31a78de55593
hellogirls-js <neeneemille@gmail.com>
2025-12-28 15:33:34 -0600

    finish writing beginner-friendly exercises

--- exercises/type_effectiveness.sql
+++ exercises/type_effectiveness.sql
@@ -7,6 +7,8 @@ effectiveness of different types and type combinations.
 You will start with retrieving the weaknesses and resistances for single types
 then move onto calculating the total type multiplier for various type combinations.
 
+All of the necessary information is stored in the type_chart table.
+
 Good luck!
 
 */
@@ -23,8 +25,19 @@ Good luck!
 
 
 
-
 -- Return the TOTAL NUMBER of types the Steel type resists.
+
+
+
+-- List every type that has an immunity. Every value must be unique.
+
+
+
+-- Return the number of weaknesses each type has. List the name and number of weaknesses.
+
+
+
+-- Build on top of this query to return the type(s) with the most weaknesses.
 
 
 
